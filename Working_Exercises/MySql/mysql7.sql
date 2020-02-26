b6d070bd113b959f8d701824938fbd12bacf39c5
Aj <ajaykuma24@gmail.com>
2020-02-26 05:22:30 +0100

    updated

---
+++ Working_Exercises/MySql/mysql7.sql
@@ -0,0 +1,9 @@
+use classicmodels;
+CREATE TABLE employees_audit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    employeeNumber INT NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    changedat DATETIME DEFAULT NULL,
+    action VARCHAR(50) DEFAULT NULL
+);
+
---
+++ Working_Exercises/MySql/mysql8.sql
@@ -0,0 +1,9 @@
+use classicmodels;
+CREATE TRIGGER before_employee_update 
+    BEFORE UPDATE ON employees
+    FOR EACH ROW 
+ INSERT INTO employees_audit
+ SET action = 'update',
+     employeeNumber = OLD.employeeNumber,
+     lastname = OLD.lastname,
+     changedat = NOW();
